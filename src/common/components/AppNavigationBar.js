3982b73b1ce4d3fd8c4481b6ecc7e9f8f4553e5f
ypconstante <ypconstante@gmail.com>
2017-10-12 20:36:56 -0300

    navbar com imagens

--- src/common/components/AppNavigationBar.js
+++ src/common/components/AppNavigationBar.js
@@ -1,17 +1,33 @@
 import React, {PureComponent} from 'react';
+import {Image, StyleSheet} from "react-native";
 import NavigationBar from 'react-native-navbar';
 
 import colors from './../../colors';
-import {StyleSheet, View} from "react-native";
 
 class AppNavigationBar extends PureComponent {
     render() {
         return (
             <NavigationBar
-                title={titleConfig}
-                rightButton={rightButtonConfig}
                 containerStyle={styles.container}
                 style={styles.navigationBar}
+                leftButton={
+                    <Image
+                        source={require('./assets/AppNavigationBar-menu.png')}
+                        style={styles.navigationBarMenu}
+                    />
+                }
+                title={
+                    <Image
+                        source={require('./assets/AppNavigationBar-logo.png')}
+                        style={styles.navigationBarLogo}
+                    />
+                }
+                rightButton={
+                    <Image
+                        source={require('./assets/AppNavigationBar-profile.png')}
+                        style={styles.navigationBarProfile}
+                    />
+                }
             />
         )
     }
@@ -20,20 +36,32 @@ class AppNavigationBar extends PureComponent {
 const styles = StyleSheet.create({
     container: {
         flex: 0,
-        paddingTop: 24,
+        marginTop: 24,
+        elevation: 3,
     },
     navigationBar: {
-      backgroundColor: colors.white,
+        backgroundColor: "#f8f8f8",
     },
+    navigationBarMenu: {
+        width: 28,
+        height: 20,
+        marginTop: 12,
+        marginLeft: 8,
+        resizeMode: 'contain',
+    },
+    navigationBarLogo: {
+        width: 184,
+        height: 34,
+        marginTop: 5,
+        resizeMode: 'contain',
+    },
+    navigationBarProfile: {
+        width: 18,
+        height: 34,
+        marginTop: 5,
+        marginRight: 8,
+        resizeMode: 'contain',
+    }
 });
-
-const rightButtonConfig = {
-    title: 'Next',
-    handler: () => alert('hello!'),
-};
-
-const titleConfig = {
-    title: 'Hello, world',
-};
 
 export default AppNavigationBar;
